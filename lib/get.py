8dedf6fc96405941a602f1ca8807dabfd518c2b1
rebeccanakhanu <rebeccanakhanu@gmail.com>
2023-06-23 22:11:32 +0300

    completed assignments

--- lib/get.py
+++ lib/get.py
@@ -4,7 +4,6 @@ import json
 url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
 
 response = requests.get(url)
+json_content= json.loads(response.text) 
+print(json.dumps(json_content,indent=4,sort_keys=True))
 
-json_content = json.loads(response.text)
-
-print(json.dumps(json_content, indent=4))
